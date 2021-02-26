ab9ff222e3d74ad7f281a2655975ab25df1c389d
tim <ts00395109@yahoo.com.tw>
2021-02-26 20:41:44 +0800

    Initial commit

---
+++ src/components/result.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    以下是{{ test}}的訂單
+    {{applecount.toString()}}顆貧果,
+    <br>
+    香蕉配料
+    <ul class="nav">
+      <li v-for="fav in favor" v-bind:key="fav">
+          {{fav}}
+      </li>
+    </ul>
+
+    <button v-on:click="printDataNOW" style="height: 50px;width: 155px">show目前data</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    test: {type: String,require:true},
+    applecount:{type:Number},
+    favor:{type:Array},
+    data_Now:{type:String}
+  },
+  data: function () {
+    return {
+
+    };
+  },
+  methods: {
+    printDataNOW: function () {
+      alert(JSON.stringify(this.data_Now));
+      console.log(JSON.stringify(this.data_Now));
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
